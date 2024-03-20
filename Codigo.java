07ff1ec9a6ef4d125f526220b3a98de9066d62ee
josephaven <153795069+josephaven@users.noreply.github.com>
2024-03-19 20:55:58 -0600

    Create Codigo.java

---
+++ Codigo.java
@@ -0,0 +1,87 @@
+interface Redimensionable {
+    void resize(double factor);
+}
+
+
+abstract class Figure {
+    protected int numSides;
+
+    public Figure(int numSides) {
+        this.numSides = numSides;
+    }
+
+    public int getNumSides() {
+        return numSides;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
+
+
+class Rectangle extends Figure implements Redimensionable {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        super(4);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public void resize(double factor) {
+        width *= factor;
+        height *= factor;
+    }
+}
+
+
+class Triangle extends Figure {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        super(3);
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return -1;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(6, 4);
+        
+        System.out.println("Area of the rectangle: " + rectangle.getArea());
+        System.out.println("Perimeter of the rectangle: " + rectangle.getPerimeter());
+        
+        rectangle.resize(5);
+        
+        System.out.println("After resizing:");
+        System.out.println("Area of the rectangle: " + rectangle.getArea());
+        System.out.println("Perimeter of the rectangle: " + rectangle.getPerimeter());
+    }
+}
+
